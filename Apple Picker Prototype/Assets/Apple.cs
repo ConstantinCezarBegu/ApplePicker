871922e07736f7cbc5d21882a3ae285dbe594c7f
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-01-22 22:04:32 -0500

    vreated objects and attached them to their scripts.

--- Apple Picker Prototype/Assets/Apple.cs
+++ Apple Picker Prototype/Assets/Apple.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
-{
+public class Apple : MonoBehaviour { 
+
+    private static float bottomY = -20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        }
     }
 }
--- Apple Picker Prototype/Assets/AppleTree.cs
+++ Apple Picker Prototype/Assets/AppleTree.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+
+    [Header("Set in	Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("dropApple", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 pos = transform.position;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+    void dropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("dropApple", secondsBetweenAppleDrops);
     }
 }
+
+
